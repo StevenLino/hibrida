5e874148c3d40cd2b6cc95cce4c1f5c9c077ca73
StevenLino <stemlino@espol.edu.ec>
2024-12-18 00:22:51 -0500

    first

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,62 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonSelect, IonSelectOption, IonTextarea, IonButton,
+  IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+/* Importe el módulo para formularios reactivos */
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+
+/* Importe el servicio */
+import { ProviderService } from '../services/provider.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [ReactiveFormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonSelect, IonSelectOption, IonTextarea, IonButton,
+    IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
 
-  constructor() {}
+  // constructor() { }
+
+  /* Instancie un formulario */
+  myForm: FormGroup = new FormGroup({
+    score: new FormControl("", Validators.required),
+    opinion: new FormControl("", Validators.required)
+  });
+
+  /* Nombre de la colección */
+  collectionName = 'reviews';
+
+  /* Inyecte la dependencia a Firestore */
+  constructor(private providerService: ProviderService) { }
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onSubmit() {
+    this.providerService.createDocument(this.collectionName, this.myForm.value).then(() => {
+      this.myForm.reset()
+    });
+  }
+
+  /* Arreglo con datos locales */
+  dataList: any[] = [];
+
+  /* Al inicializar, carga los datos  */
+  ngOnInit() {
+      this.loadData();
+  }
+
+  loadData() {
+      this.providerService.readCollection(this.collectionName).subscribe((data) => {
+          this.dataList = data;
+      });
+  }
+
 
 }

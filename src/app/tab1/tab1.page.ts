2e7b2a5d7ff0f0c63b7513d434b5d871abb0ba48
StevenLino <stemlino@espol.edu.ec>
2024-12-18 01:20:23 -0500

    Cambios guía 18 y 19

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,78 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ViewChild, ElementRef, Component, signal } from '@angular/core';
+import { IonCardContent, IonButton, IonList, IonItem, IonLabel, IonFab, IonFabButton, IonIcon, IonCard, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+/* Importe la función y el ícono */
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
+
+/* Importe el servicio */
+import { TeachablemachineService } from '../services/teachablemachine.service';
+
+/* Importe el pipe */
+import { PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [PercentPipe, IonCardContent, IonButton, IonList, IonItem, IonLabel, IonFab, IonFabButton, IonIcon, IonCard, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  /* Declare la referencia al elemento con el id image */
+  @ViewChild('image', { static: false }) imageElement!: ElementRef<HTMLImageElement>;
+
+  /* Lista de predicciones */
+  predictions: any[] = [];
+
+  /* Método para obtener la predicción a partir de la imagen */
+  async predict() {
+    try {
+      const image = this.imageElement.nativeElement;
+      this.predictions = await this.teachablemachine.predict(image);
+    } catch (error) {
+      console.error(error);
+      alert('Error al realizar la predicción.');
+    }
+  }
+
+  imageReady = signal(false)
+  imageUrl = signal("")
+
+  /* Declare los atributos para almacenar el modelo y la lista de clases */
+  modelLoaded = signal(false);
+  classLabels: string[] = [];
+
+  constructor(private teachablemachine: TeachablemachineService) {
+    /* Registre el ícono */
+    addIcons({ cloudUploadOutline });
+  }
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+
+      const reader = new FileReader();
+
+      // Convertir el archivo a una URL base64 para mostrarlo en el html
+      reader.onload = () => {
+        this.imageUrl.set(reader.result as string)
+        this.imageReady.set(true)
+      };
+
+      reader.readAsDataURL(file); // Leer el archivo como base64
+    }
+  }
+
+  /* Método ngOnInit para cargar el modelo y las clases */
+  async ngOnInit() {
+    await this.teachablemachine.loadModel()
+    this.classLabels = this.teachablemachine.getClassLabels()
+    this.modelLoaded.set(true)
+  }
 }

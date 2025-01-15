297208c2be4837a607be89d16b2f7b5f7995fe12
Angel Ariel Gómez Tumbaco <anargome@espol.edu.ec>
2025-01-15 03:29:21 -0500

    cambios clasificador

--- src/app/services/teachablemachine.service.ts
+++ src/app/services/teachablemachine.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TeachablemachineService {
-  private URL = "https://teachablemachine.withgoogle.com/models/cXiScRib7/"
+  private URL = "https://teachablemachine.withgoogle.com/models/l2ryJ8WNs/"
   private model: any;
   private classLabels: string[] = [];
 

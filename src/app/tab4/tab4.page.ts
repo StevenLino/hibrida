996730b605ad4c110360ec358fc0615fd4b21a99
Angel Ariel Gómez Tumbaco <116834047+Kenkyo1@users.noreply.github.com>
2025-01-19 09:06:57 -0500

    cambios en tabs

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,21 +2,14 @@ import { Component } from '@angular/core';
 import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
+
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
   standalone: true,
   imports: [IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar, IonGrid, IonRow, IonCol],
 })
-export class Tab3Page {
-  categorias = [
-    { titulo: 'Camisa', imagen: 'https://example.com/camisa.jpg' },
-    { titulo: 'Camiseta', imagen: 'https://example.com/camiseta.jpg' },
-    { titulo: 'Pantalón', imagen: 'https://example.com/pantalon.jpg' },
-    { titulo: 'Pantaloneta', imagen: 'https://example.com/pantaloneta.jpg' },
-    { titulo: 'Gorra', imagen: 'https://example.com/gorra.jpg' },
-    { titulo: 'Calzado', imagen: 'https://example.com/calzado.jpg' },
-  ];
+export class Tab4Page {
   constructor() {}
 }

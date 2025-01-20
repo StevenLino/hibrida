2d4f168e4bbce89dc65079eab5f706f256014c4a
Angel Ariel Gómez Tumbaco <anargome@espol.edu.ec>
2025-01-20 04:30:29 -0500

    cambios en el icon

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonAvatar, IonItem, IonText } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { personSharp, rocket, clipboardOutline, peopleCircle, triangle, ellipse, square, library, addCircleOutline } from 'ionicons/icons';
+import { camera, personSharp, rocket, clipboardOutline, peopleCircle, triangle, ellipse, square, library, addCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ personSharp, rocket, clipboardOutline, peopleCircle, triangle, ellipse, square, library, addCircleOutline });
+    addIcons({ camera, personSharp, rocket, clipboardOutline, peopleCircle, triangle, ellipse, square, library, addCircleOutline });
   }
 }
